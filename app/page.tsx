8d670093f963a58fd64fa80962ae1bada076afa8
Yazid9899 <yazidsiddiq9899@gmail.com>
2023-11-03 08:05:06 +0700

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import CityPicker from "@/components/CityPicker";
-import { Card, Subtitle, Text, Divider } from "@tremor/react";
+import { Card, Subtitle, Text, Divider, Title } from "@tremor/react";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7B] p-10 flex-col justify-center">
       <Card className="max-w-4xl mx-auto mt-10">
-        <Text className="text-[3.5rem] font-bold text-center mb-10">Weather Radar</Text>
+        <Title className="text-[3.5rem] font-bold text-center mb-10">Weather Radar</Title>
         <Subtitle className="text-xl text-center">
           Powered By next.js, Tailwind css, Tremor 2.0 + More!
         </Subtitle>
